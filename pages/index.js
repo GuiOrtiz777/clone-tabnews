773cecf5a0fe33efccfc68447da90f86ee93ba8a
Guilherme Ortiz <gui.ortiz@live.com>
2026-01-30 00:37:56 +0000

    Adiciona script lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Mozão, estou estudando e pensei em você. Só quero dizer que amei acordar mais um dia ao seu lado. E amei fazer a massagem em você também 🥰 </h1>
+  return (
+    <h1>
+      Mozão, estou estudando e pensei em você. Só quero dizer que amei acordar
+      mais um dia ao seu lado. E amei fazer a massagem em você também 🥰{" "}
+    </h1>
+  );
 }
 
 export default Home;
